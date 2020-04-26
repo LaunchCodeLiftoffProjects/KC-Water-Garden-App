b1cde013945687c55232d8332da20378138c9874
Jamie Andersen <jamieandersen29@gmail.com>
2020-04-26 15:49:09 -0500

    registration error when transaction to repository

--- src/main/java/org/launchcode/water_garden_tour/models/data/UserRepository.java
+++ src/main/java/org/launchcode/water_garden_tour/models/data/UserRepository.java
@@ -3,17 +3,14 @@ package org.launchcode.water_garden_tour.models.data;
 import org.launchcode.water_garden_tour.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
-<<<<<<< HEAD
 
-@Repository
-
-=======
 import javax.transaction.Transactional;
+import java.util.Optional;
+
 
 @Repository
 @Transactional
->>>>>>> 0652212... Created a custom login form, login controller & updated Spring Security settings to use our login form. Added the new User fields to UserDetailsImplementation. Updated security configuration to properly implement password encoder using DaoAuthenticationProvider.
+
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
 }
