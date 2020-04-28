d0587c3e75cb8efc8bec6f7ee28ec17dff064480
Heather Strodtman <46832814+hstrodtman@users.noreply.github.com>
2020-04-27 19:47:25 -0500

    Spring security (#1)
    * Added bcrypt password encoder & refactored UserDetailsService loadUserByUsername method.
    
    * Added bcrypt password encoder & refactored UserDetailsService loadUserByUsername method.
    
    * Created a custom login form, login controller & updated Spring Security settings to use our login form. Added the new User fields to UserDetailsImplementation. Updated security configuration to properly implement password encoder using DaoAuthenticationProvider.
    
    * Updated log out nav link to post method form submission in fragments.html. Added thymeleaf-extras-springsecurity5 configuration to dependencies & xmls:sec info to the fragments.html to add checks for authentication to view certain nav links. Updated http security config to permit all access to logout form. Added fragments to the login.html.

--- src/main/java/org/launchcode/water_garden_tour/SecurityConfiguration.java
+++ src/main/java/org/launchcode/water_garden_tour/SecurityConfiguration.java
@@ -2,43 +2,42 @@ package org.launchcode.water_garden_tour;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.sql.DataSource;
-
+@Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    DataSource dataSource;
-
-    @Autowired
     UserDetailsService userDetailsService;
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-    // To enable Spring Data in Spring Security https://www.baeldung.com/spring-data-security
-    // "enables activation of automatic resolving of spring-data specific expressions annotated on classes"
-//    @Bean
-//    public SecurityEvaluationContextExtension securityEvaluationContextExtension() {
-//        return new SecurityEvaluationContextExtension();
-//    }
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+        auth.setUserDetailsService(userDetailsService);
+        auth.setPasswordEncoder(passwordEncoder());
+        return auth;
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
-
+        auth.authenticationProvider(authenticationProvider());
     }
-        @Bean
-            public PasswordEncoder getPasswordEncoder() {
-            return NoOpPasswordEncoder.getInstance();
-        }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -47,7 +46,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/user").hasAnyRole("USER", "ADMIN")
                 .antMatchers("/").permitAll()
-                .and().formLogin();
+                .and().formLogin().loginPage("/login").permitAll()
+                .and().logout().permitAll();
     }
-
 }
