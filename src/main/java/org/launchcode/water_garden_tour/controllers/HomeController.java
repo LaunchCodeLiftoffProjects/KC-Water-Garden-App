6d7f1f748628278fa8167a8f3c24fa8c2e7bf308
MulesKC <rmmcp3@gmail.com>
2020-04-28 20:29:56 -0500

    re-added HomeController.java, removed extra html element from fragments.html

---
+++ src/main/java/org/launchcode/water_garden_tour/controllers/HomeController.java
@@ -0,0 +1,38 @@
+package org.launchcode.water_garden_tour.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("")
+    public String index(Model model) {
+
+        model.addAttribute("title","Kansas City Water Garden Tours");
+
+        return "index";
+    }
+
+
+    @GetMapping("/home")
+    @ResponseBody
+    public String home() {
+        return "Welcome";
+    }
+
+    @GetMapping("/user")
+    @ResponseBody
+    public String user() {
+        return "Welcome User";
+    }
+
+    @GetMapping("/admin")
+    @ResponseBody
+    public String hello() {
+        return "Welcome Admin";
+    }
+
+}
