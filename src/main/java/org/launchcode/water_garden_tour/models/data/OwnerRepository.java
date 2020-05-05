c965ed9e84e13bc0aba237d23fee48395da4b6f2
Ryan Mueller <RMuellerKC@gmail.com>
2020-05-04 20:15:52 -0500

    Garden List initial build (#5)
    * initial build of models: Feature, Garden, GardenDetails, GardenOwner
    
    * added initial GardenList templates: features/add&list, owner/add&list, and garden/add&list
    
    * updated page-header in fragments.html(fixed logo, added register), updated index.html to use page-header fragment, removed @Entity from models to get program to run (classes weren't complete yet)
    
    * combined Jamie and my initial work on Garden List including: adding Feature,Garden,Owner repositories, removed GardenDetails, updated references to GardenDetails
    
    * added to SecurityConfiguration to post to MySQL, further adjusting templates, controllers, and models to get entry of new features, owners, and gardens working - garden creation not quite finished
    
    * database working correctly; Garden, owner, and feature listing working
    
    * updated gardens/add for latitude & longitude; added validation to some fields in Garden.java
    
    * added '.' for img-fluid in css; removed my quick-fix in SecurityConfig; updated page-header fragment with garden, features, and owners links; added link to 'add garden' on garden list page
    
    * fixed linking to Garden list; added back some code to SecurityConfig (**line 56**) to get database working again; made latitude/longitude strings for now* fixed linking to Garden list; added back some code to SecurityConfig to get database working again; made latitude/longitude strings for now
    
    * removed css folder, put css in static; fixed add owner link on add garden page; fixed small syntax issues on garden list

---
+++ src/main/java/org/launchcode/water_garden_tour/models/data/OwnerRepository.java
@@ -0,0 +1,13 @@
+package org.launchcode.water_garden_tour.models.data;
+
+import org.launchcode.water_garden_tour.models.garden.Owner;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
+public interface OwnerRepository extends JpaRepository<Owner, Integer> {
+
+}
