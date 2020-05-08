385190d81d4e553e1cd1bbcc8c1c27d1d5a64de7
Scott Penneman <36176977+scottpenneman@users.noreply.github.com>
2020-05-07 20:24:39 -0500

    Add garden locations to map (#8)
    * updated garden controller with jsonignore annotation to prevent infinite recursion, updated styles.css with map styling, updated map with thymeleaf javascript to add markers from gardenRepository

--- src/main/java/org/launchcode/water_garden_tour/controllers/MapController.java
+++ src/main/java/org/launchcode/water_garden_tour/controllers/MapController.java
@@ -1,5 +1,7 @@
 package org.launchcode.water_garden_tour.controllers;
 
+import org.launchcode.water_garden_tour.models.data.GardenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MapController {
 
+    @Autowired
+    GardenRepository gardenRepository;
+
     @RequestMapping("map")
     public String map(Model model) {
-
-        model.addAttribute("title","Kansas City Water Garden Tours");
+        model.addAttribute("gardens", gardenRepository.findAll());
         return "map";
     }
 
