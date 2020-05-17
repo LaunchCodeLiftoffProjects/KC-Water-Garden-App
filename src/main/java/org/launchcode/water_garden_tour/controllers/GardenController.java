91bcf28a7593a55cc3c4f33835ede3b2e6bba5ae
MulesKC <rmmcp3@gmail.com>
2020-05-17 06:03:59 -0500

    updated gardens/list and view to Heather's latest; fixed bug with not selecting a feature during garden updates
    finished search feature and category filter feature; fixed bug heather found w/ viewing owners w/o any gardens in db
    
    fixing bugs with routing
    
    fixed titling of various sites, got /admin to disappear from navbar when active site, removed search.html - not used
    
    added view gardens by feature to index.html; updated HomeController and GardenController as needed for this addition
    
    added /user/ to several links in various templates
    
    small html updates; removed several else statements from GardenData
    
    fixed merge conflicts in index and gardens/add

--- src/main/java/org/launchcode/water_garden_tour/controllers/GardenController.java
+++ src/main/java/org/launchcode/water_garden_tour/controllers/GardenController.java
@@ -1,22 +1,27 @@
 package org.launchcode.water_garden_tour.controllers;
 
-
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.launchcode.water_garden_tour.models.data.FeatureRepository;
 import org.launchcode.water_garden_tour.models.data.GardenRepository;
 import org.launchcode.water_garden_tour.models.data.OwnerRepository;
+import org.launchcode.water_garden_tour.models.garden.Feature;
 import org.launchcode.water_garden_tour.models.garden.Garden;
+import org.launchcode.water_garden_tour.models.garden.GardenData;
+import org.launchcode.water_garden_tour.models.garden.Owner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -34,11 +39,75 @@ public class GardenController {
 
     //****Garden controllers
 
-    @GetMapping("/gardens/list")
-    public String listGarden(Model model) {
+    @GetMapping("/gardens/add")
+    public String renderAddGarden(Model model) {
+        model.addAttribute("title", "Add Garden");
+        model.addAttribute("owners", ownerRepository.findAll());
+        model.addAttribute("features", featureRepository.findAll());
+        model.addAttribute(new Garden());
+        return "gardens/add";
+    }
 
+    @PostMapping("/gardens/add")
+    public String addGarden(Model model, @ModelAttribute @Valid Garden newGarden,
+                            Errors errors, @RequestParam("file") MultipartFile file) throws IOException {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Garden");
+            return "gardens/add";
+        }
+
+        newGarden.setImage(file.getBytes());
+      
+        gardenRepository.save(newGarden);
+
+        List<Feature> selectedFeatures = new ArrayList<>();
+
+        model.addAttribute("selectedFeatures", selectedFeatures);
         model.addAttribute("gardens", gardenRepository.findAll());
+        model.addAttribute("features", featureRepository.findAll());;
         model.addAttribute("title", "Garden List");
+        model.addAttribute("searchTerm", "");
+
+        return "/gardens/list";
+    }
+
+    @GetMapping("gardens/list")
+    public String listGarden(Model model) {
+        List<Feature> selectedFeatures = new ArrayList<>();
+
+        model.addAttribute("selectedFeatures", selectedFeatures);
+        model.addAttribute("gardens", gardenRepository.findAll());
+        model.addAttribute("features", featureRepository.findAll());;
+        model.addAttribute("title", "Garden List");
+        model.addAttribute("searchTerm", "");
+
+        return "gardens/list";
+    }
+
+    @GetMapping("gardens/search")
+    public String renderGardenSearch(Model model, @RequestParam(required=false, value="searchTerm") String searchTerm, @RequestParam(required = false, value="featureIds") Integer[] featureIds) {
+        Iterable<Garden> gardens;
+        List<Feature> allFeatures = featureRepository.findAll();
+        List<Feature> selectedFeatures = new ArrayList<>();
+
+        if (featureIds != null) {
+            gardens = GardenData.gardenSearchFeaturesFiltered(searchTerm, featureIds, allFeatures, gardenRepository.findAll());
+
+            for (int id : featureIds) {
+                selectedFeatures.add(GardenData.getFeatureFromId(id, allFeatures));
+            }
+
+        } else {
+            gardens = GardenData.gardenSearchAllFeatures(searchTerm, allFeatures, gardenRepository.findAll());
+
+        }
+
+        model.addAttribute("gardens", gardens);
+        model.addAttribute("selectedFeatures", selectedFeatures);
+        model.addAttribute("features", featureRepository.findAll());
+        model.addAttribute("searchTerm", searchTerm);
+        model.addAttribute("title", "Garden Search");
 
         return "gardens/list";
     }
@@ -55,7 +124,341 @@ public class GardenController {
         IOUtils.copy(is, response.getOutputStream());
     }
 
-    @GetMapping("view/{gardenId}")
+    @GetMapping("/gardens/feature/{featureId}")
+    public String renderGardenViewByFeature(Model model, @PathVariable int featureId) {
+        List<Garden> gardensWithFeature = new ArrayList<>();
+        List<Feature> curFeatures = new ArrayList<>();
+
+        Feature featureForView = GardenData.getFeatureFromId(featureId, featureRepository.findAll());
+
+
+        for (Garden garden : gardenRepository.findAll()) {
+            curFeatures = garden.getFeatures();
+            if (curFeatures.contains(featureForView)) {
+                if (!gardensWithFeature.contains(garden)) {
+                    gardensWithFeature.add(garden);
+                }
+            }
+        }
+
+
+        String title = featureForView + "View";
+
+
+        model.addAttribute("gardens", gardensWithFeature);
+        model.addAttribute("feature", featureForView);
+        model.addAttribute("title", title);
+
+        return "gardens/feature-view";
+    }
+
+    @GetMapping("/gardens/admin-list")
+    public String listAdminGarden(Model model) {
+        model.addAttribute("gardens", gardenRepository.findAll());
+        model.addAttribute("title", "Garden ADMIN List");
+
+        return "gardens/admin-list";
+    }
+
+    @PostMapping("/gardens/delete")
+    public String deleteGarden(Model model, @RequestParam int gardenId) {
+
+        Optional optGarden = gardenRepository.findById(gardenId);
+        if (optGarden.isPresent()) {
+            Garden garden = (Garden) optGarden.get();
+
+            gardenRepository.delete(garden);
+        }
+
+        model.addAttribute("gardens", gardenRepository.findAll());
+        model.addAttribute("title", "Garden ADMIN List");
+
+        return "gardens/admin-list";
+    }
+
+    @GetMapping("/gardens/update")
+    public String renderUpdateGarden(Model model, @RequestParam(value = "gardenId") int gardenId) {
+
+        Optional optGarden = gardenRepository.findById(gardenId);
+        if (optGarden.isPresent()) {
+            Garden gardenToUpdate = (Garden) optGarden.get();
+            model.addAttribute("garden", gardenToUpdate);
+
+            List<Feature> gardenFeatures = gardenToUpdate.getFeatures();
+
+            Owner gardenOwner = gardenToUpdate.getOwner();
+            int ownerId = gardenOwner.getId();
+
+            model.addAttribute("gardenOwner", gardenOwner);
+            model.addAttribute("gardenFeatures", gardenFeatures);
+        }
+        model.addAttribute("title", "Update Garden");
+        model.addAttribute("features", featureRepository.findAll());
+        model.addAttribute("owners", ownerRepository.findAll());
+
+        return "gardens/update";
+    }
+    //TODO add validation here?
+    @PostMapping("/gardens/update")
+    public String updateGarden(Model model,
+                               int gardenId,
+                               @RequestParam("file") MultipartFile file,
+                               String name,
+                               String address,
+                               String latitude,
+                               String longitude,
+                               String description,
+                               int ownerId,
+                               Integer[] featureIds) throws IOException {
+
+        Optional<Garden> optGarden = gardenRepository.findById(gardenId);
+        Garden gardenToUpdate = (Garden) optGarden.get();
+
+        Optional<Owner> optOwner = ownerRepository.findById(ownerId);
+        Owner owner = (Owner) optOwner.get();
+
+        if (featureIds != null) {
+            List<Feature> features = new ArrayList<>();
+            for (Integer id : featureIds) {
+
+                Optional<Feature> optFeature = featureRepository.findById(id);
+                Feature featureToAdd = (Feature) optFeature.get();
+
+                features.add(featureToAdd);
+            }
+
+            gardenToUpdate.setFeatures(features);
+
+        }
+
+
+        gardenToUpdate.setName(name);
+        gardenToUpdate.setAddress(address);
+        gardenToUpdate.setLatitude(latitude);
+        gardenToUpdate.setLongitude(longitude);
+        gardenToUpdate.setDescription(description);
+        if(!file.isEmpty()) {
+            gardenToUpdate.setImage(file.getBytes());
+        }
+        gardenToUpdate.setOwner(owner);
+
+        gardenRepository.save(gardenToUpdate);
+
+        //attributes for return to admin-list
+        model.addAttribute("gardens", gardenRepository.findAll());
+        model.addAttribute("title", "Garden ADMIN List");
+
+        return "gardens/admin-list";
+    }
+
+    //****Feature Controllers
+
+    @GetMapping("/features/add")
+    public String renderAddFeature(Model model) {
+        model.addAttribute("title", "Add Feature");
+        model.addAttribute(new Feature());
+        return "features/add";
+    }
+
+    @PostMapping("/features/add")
+    public String addFeature(@ModelAttribute @Valid Feature newFeature,
+                             Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            return "/features/add";
+        }
+
+        featureRepository.save(newFeature);
+
+        return "redirect:";
+    }
+
+    @GetMapping("/features")
+    public String listFeature(Model model) {
+        model.addAttribute("features", featureRepository.findAll());
+        model.addAttribute("title", "Feature List");
+
+        return "/features/list";
+    }
+
+    @PostMapping("/features/delete")
+    public String deleteFeature(Model model, @RequestParam int featureId) {
+
+        Optional optFeature = featureRepository.findById(featureId);
+        if (optFeature.isPresent()) {
+            Feature featureToDelete = (Feature) optFeature.get();
+
+            List<Garden> gardens = gardenRepository.findAll();
+            for (Garden garden : gardens) {
+                List<Feature> curGardenFeatures = garden.getFeatures();
+                if (curGardenFeatures.contains(featureToDelete)) {
+                    curGardenFeatures.remove(featureToDelete);
+
+                    garden.setFeatures(curGardenFeatures);
+                    gardenRepository.save(garden);
+                }
+            }
+
+            featureRepository.delete(featureToDelete);
+        }
+
+        //attributes for return to admin-list
+        model.addAttribute("gardens", gardenRepository.findAll());
+        model.addAttribute("title", "Garden ADMIN List");
+
+        return "gardens/admin-list";
+    }
+
+    @GetMapping("/features/update")
+    public String renderUpdateFeature(Model model, @RequestParam(value = "featureId") int featureId) {
+
+        Optional optFeature = featureRepository.findById(featureId);
+        if (optFeature.isPresent()) {
+            Feature featureToUpdate = (Feature) optFeature.get();
+            model.addAttribute("feature", featureToUpdate);
+        }
+
+        model.addAttribute("title", "Update Feature");
+
+        return "features/update";
+    }
+    //TODO add validation here?
+    @PostMapping("/features/update")
+    public String updateFeature(Model model,
+                              int featureId,
+                              String name) {
+
+        Optional<Feature> optFeature = featureRepository.findById(featureId);
+        Feature featureToUpdate = (Feature) optFeature.get();
+
+        featureToUpdate.setName(name);
+
+        featureRepository.save(featureToUpdate);
+
+        //attributes for return to owner list
+        model.addAttribute("features", featureRepository.findAll());
+        model.addAttribute("title", "Feature List");
+
+        return "features/list";
+    }
+
+    //****Owner Controllers
+
+    @GetMapping("/owners/add")
+    public String renderAddOwner(Model model) {
+        model.addAttribute("title", "Add Owner");
+        model.addAttribute(new Owner());
+        return "owners/add";
+    }
+
+    @PostMapping("/owners/add")
+    public String addGarden(@ModelAttribute @Valid Owner newOwner,
+                            Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            return "owners/add";
+        }
+
+        ownerRepository.save(newOwner);
+
+        return "redirect:";
+    }
+
+    @PostMapping("/owners/delete")
+    public String deleteOwner(Model model, @RequestParam int ownerId) {
+
+        Optional optOwner = ownerRepository.findById(ownerId);
+        if (optOwner.isPresent()) {
+            Owner ownerToDelete = (Owner) optOwner.get();
+
+            ownerRepository.delete(ownerToDelete);
+        }
+
+        //attributes for return to admin-list
+        model.addAttribute("gardens", gardenRepository.findAll());
+        model.addAttribute("title", "Garden ADMIN List");
+
+        return "gardens/admin-list";
+
+    }
+
+    @GetMapping("/owners")
+    public String listOwner(Model model) {
+        model.addAttribute("title", "Owners List");
+        model.addAttribute("owners", ownerRepository.findAll());
+
+        //delete button only for owners not attached to gardens
+        List<Garden> gardens = gardenRepository.findAll();
+        List<Owner> ownersWithGardens = new ArrayList<>();
+
+        for (Garden garden : gardens) {
+            Optional optGardenOwner = ownerRepository.findById(garden.getOwner().getId());
+            if (optGardenOwner.isPresent()) {
+                Owner curOwner = (Owner) optGardenOwner.get();
+
+                ownersWithGardens.add(curOwner);
+            }
+        }
+
+        model.addAttribute("ownersWithGardens", ownersWithGardens);
+
+        return "owners/list";
+    }
+
+    @GetMapping("/owners/update")
+    public String renderUpdateOwner(Model model, @RequestParam(value = "ownerId") int ownerId) {
+
+        Optional optOwner = ownerRepository.findById(ownerId);
+        if (optOwner.isPresent()) {
+            Owner ownerToUpdate = (Owner) optOwner.get();
+            model.addAttribute("owner", ownerToUpdate);
+        }
+
+        model.addAttribute("title", "Update Owner");
+
+        return "owners/update";
+    }
+    //TODO add validation here?
+    @PostMapping("/owners/update")
+    public String updateOwner(Model model,
+                               int ownerId,
+                               String name,
+                               String email,
+                               String phoneNum) {
+
+        Optional<Owner> optOwner = ownerRepository.findById(ownerId);
+        Owner ownerToUpdate = (Owner) optOwner.get();
+
+        ownerToUpdate.setName(name);
+        ownerToUpdate.setEmail(email);
+        ownerToUpdate.setPhoneNum(phoneNum);
+
+        ownerRepository.save(ownerToUpdate);
+
+        //attributes for return to owner list
+        model.addAttribute("owners", ownerRepository.findAll());
+        model.addAttribute("title", "Owner List");
+
+        //delete button only for owners not attached to gardens
+        List<Garden> gardens = gardenRepository.findAll();
+        List<Owner> ownersWithGardens = new ArrayList<>();
+
+        for (Garden garden : gardens) {
+            Optional optGardenOwner = ownerRepository.findById(garden.getOwner().getId());
+            if (optGardenOwner.isPresent()) {
+                Owner curOwner = (Owner) optGardenOwner.get();
+
+                ownersWithGardens.add(curOwner);
+            }
+
+            model.addAttribute("ownersWithGardens", ownersWithGardens);
+
+        }
+
+        return "owners/list";
+    }
+
+    @GetMapping("/view/{gardenId}")
     public String viewGarden(Model model, @PathVariable int gardenId) {
         Optional<Garden> foundGarden = gardenRepository.findById(gardenId);
         Garden garden = foundGarden.get();
